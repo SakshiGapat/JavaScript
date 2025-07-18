3ab0d3c26cb46112c9a18821473198225cefba8d
SakshiGapat <sakshigapat2004@gmail.com>
2025-07-18 23:47:47 +0530

    Assignment on Datatypes in JavaScript

---
+++ 02-data-types.js
@@ -0,0 +1,23 @@
+
+console.log("========== Positive Datatypes =========="); 
+var city = "Pune";
+var result = typeof city;
+console.log("City: ",city,"type of city: ", result);
+
+var firstName = 'Elon';
+var result = typeof firstName;
+console.log("First Name: ", firstName,"It's type: ", result);
+
+var pinCode="411048";
+var result = typeof pinCode;
+console.log("Pincode: ",pinCode,"And iy's data type: ",result);
+
+console.log("============================");
+var age = 20;
+var typeOfAge = typeof age;
+console.log("Age ", age, "and it's data type is: ", typeOfAge);
+
+console.log("============================");
+var isMarried = false;
+var typeOfIsMarried = typeof isMarried;
+console.log("Is Married: ",isMarried, " and it's DT is: ", typeOfIsMarried);
