4350fb19dc8b5672e2abecec7a41cc2ed45b0000
SakshiGapat <sakshigapat2004@gmail.com>
2025-07-18 23:47:19 +0530

    Assignment on Variables in JavaScript

---
+++ 01-variable-assignment.js
@@ -0,0 +1,46 @@
+var myName; //variable declaration
+console.log("myName variable declaration:",myName);
+myName = "Sakshi"; //variable initialization
+console.log("myName variable initialization:",myName);
+
+
+var myName = "Sakshi";
+console.log("My Name:",myName);
+
+myName = "Vaishnavi";
+console.log("myName updated value:",myName);
+
+myName = "Siddhi";
+console.log("myName updated Value:",myName);
+
+var myFullName ;
+console.log("myFullName Vairiable declaration:",myFullName);
+var myFullName = "Sakshi Gapat";
+console.log("My Full Name is:",myFullName);
+var myFullName = "Sakshi Dilip Gapat";
+console.log("My Updated Full Name is:",myFullName);
+
+var myLove = "I love only JS";
+console.log("My Love is:",myLove);
+
+var firstName = "Sakshi";
+console.log("My First Name is:",firstName);
+var lastName = "Gapat";
+console.log("My Last Name is:",lastName);
+var age = "21 years";
+console.log("My Age is:",age);
+var gender = "Female";
+console.log("My Gender is:",gender);
+var isMarried = "UnMarried";
+console.log("Marital status:",isMarried);
+
+var myCity = "Pune";
+console.log("My City Name is:",myCity);
+var bestfrndName = "Vaishnavi";
+console.log("My Best Friend Name is:",bestfrndName);
+var countryName = "India";
+console.log("My Country Name is:",countryName);
+var schoolName = "SPCK";
+console.log("My School Name is:",schoolName);
+var collegeName = "TCOER";
+console.log("My College Name is:",collegeName);
