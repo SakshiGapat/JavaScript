43143b6f2245cf82766d725a351537bc46acbae0
SakshiGapat <sakshigapat2004@gmail.com>
2025-07-24 22:01:46 +0530

    NaN concept

---
+++ 05-Nan.js
@@ -0,0 +1,7 @@
+
+ console.log(`=============== NaN ===============`);
+    console.log(0/0);  //NaN
+
+    var num = "Jenny";
+    var result = +num;
+    console.log(`Value: ${result} and it's type: ${typeof result}`);
