ebdcd00bde7b224f989c692f64d4d299dfbdf682
SakshiGapat <sakshigapat2004@gmail.com>
2025-07-28 21:30:58 +0530

    While Loops in JS

---
+++ 08-while-loop.js
@@ -0,0 +1,60 @@
+
+console.log(`============ Table of 10 ============`);
+var index = 1;
+while (index<=10){
+    console.log(index*10);
+    index++;
+}
+
+console.log(`============ do while loop ============`);
+
+var index = 1;
+do {
+    console.log(index);
+    index++;
+} while (index<=10);
+
+console.log(`============ Assignment on While Loop ============`);
+
+console.log(`============ Numbers btn 5 to 15 ============`);
+var index = 5;
+while (index <=15){
+    console.log(index);
+    index++;
+}
+console.log(`============ Number btn 50 to 40 reverse ============`);
+var index = 50;
+while(index >= 40){
+    console.log(index);
+    index--;
+}
+console.log(`============ First 15 odd num ============`);
+var index = 1;
+while (index <= 30){
+    console.log(index);
+    index += 2;
+}
+console.log(`============ First 10 even num ============`);
+var index = 2;
+while (index <= 20){
+    console.log(index);
+    index += 2;
+}
+console.log(`============ Table of 5 ============`);
+var index = 1;
+while (index <= 10){
+    console.log(index*5);
+    index++;
+}
+console.log(`============ Table of 10 ============`);
+var index = 1;
+while (index <= 10){
+    console.log(index*10);
+    index++;
+}
+console.log(`============ Table of 10 in Reverse order ============`);
+var index = 10;
+while (index >= 1){
+    console.log(index*10);
+    index--;
+}
