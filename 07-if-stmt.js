c977d41f6a9df13183f0c874e42f645cc3c9c390
SakshiGapat <sakshigapat2004@gmail.com>
2025-07-28 21:30:34 +0530

    If else statements in JS

---
+++ 07-if-stmt.js
@@ -0,0 +1,18 @@
+
+console.log(`======== if stmt ========`);
+
+//When condition is true
+var num = 2;
+if (num > 0) {
+    console.log(`Bhai number 0 se bada hai`);
+    
+}
+console.log(`====== End ======`);
+
+//when condition is false
+var num = 0;
+if (num > 0) {
+    console.log(`Bhai number 0 se bada hai`);
+    
+}
+console.log(`====== End ======`);
